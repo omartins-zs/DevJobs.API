15d17041723a27fa911d81f1372c8f548ebe0dd9
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-09 15:36:36 -0300

    Ajustando Update Job Vacancy

--- Models/UpdateJobVacancyInputModel.cs
+++ Models/UpdateJobVacancyInputModel.cs
@@ -1,6 +1,6 @@
 namespace DevJobs.API.Models
 {
-    public record UpdateJobVacancyInputModel(string title, string description)
+    public record UpdateJobVacancyInputModel(string Title, string Description)
     {
 
     }
