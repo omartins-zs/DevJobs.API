ba4043d823a76407c0caed668aacf0e3eb2b2681
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-09 15:42:28 -0300

    Ajustando Add Job e Update e identação

--- Controllers/JobVacanciesController.cs
+++ Controllers/JobVacanciesController.cs
@@ -1,42 +1,73 @@
 namespace DevJobs.API.Controllers
 {
+    using DevJobs.API.Entities;
     using DevJobs.API.Models;
+    using DevJobs.API.Persistence;
     using Microsoft.AspNetCore.Mvc;
 
     [Route("api/job-vacancies")]
     [ApiController]
     public class JobVacanciesController : ControllerBase
     {
+
+        private readonly DevJobsContext _context;
+        public JobVacanciesController(DevJobsContext context)
+        {
+            _context = context;
+        }
+
         // api/JobVancancies ou api/job-vacancies
         [HttpGet]
         public ActionResult GetAll()
         {
+            var jobVacancies = _context.JobVacancies;
 
-            return Ok();
+            return Ok(jobVacancies);
         }
 
         // GET BY ID api/job-vacancies/4
         [HttpGet("{id}")]
-        public ActionResult GetById(int Id)
+        public ActionResult GetById(int id)
         {
+            var jobVacancy = _context.JobVacancies.SingleOrDefault(jv => jv.Id == id);
 
-            return Ok();
+            if (jobVacancy == null)
+                return NotFound();
+
+            return Ok(jobVacancy);
         }
 
         // POST api/job-vacancies
         [HttpPost]
         public ActionResult Post(AddJobVacancyInputModel model)
         {
+            var jobVacancy = new JobVacancy(
+                model.Title,
+                model.Description,
+                model.Company,
+                model.IsRemote,
+                model.SalaryRange
+            );
+            _context.JobVacancies.Add(jobVacancy);
 
-            return Ok();
+            return CreatedAtAction("GetById",
+            new { id = jobVacancy.Id },
+            jobVacancy);
         }
 
         // PUT api/job-vacancies/4
         [HttpPut("{id}")]
-        public ActionResult Put(UpdateJobVacancyInputModel model)
+        public ActionResult Put(int id, UpdateJobVacancyInputModel model)
         {
+            var jobVacancy = _context.JobVacancies.SingleOrDefault(jv => jv.Id == id);
+
+            if (jobVacancy == null)
+
+                return NotFound();
+            jobVacancy.Update(model.Title, model.Description);
 
             return NoContent();
+
         }
     }
 }
