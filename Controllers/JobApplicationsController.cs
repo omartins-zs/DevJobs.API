19091e546e70eb092e50849efd0b6370ab4f0fc0
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-09 15:26:11 -0300

    Ligando vaga e aplicação da vaga com o BD context

--- Controllers/JobApplicationsController.cs
+++ Controllers/JobApplicationsController.cs
@@ -1,17 +1,39 @@
 namespace DevJobs.API.Controllers
 {
+    using DevJobs.API.Entities;
     using DevJobs.API.Models;
+    using DevJobs.API.Persistence;
     using Microsoft.AspNetCore.Mvc;
 
-// Rota diferente Por que para criar um Application tem que ter um identificador
+    // Rota diferente Por que para criar um Application tem que ter um identificador
     [Route("api/job-vacancies/{id}/applications")]
     [ApiController]
     public class JobApplicationsController : ControllerBase
     {
+        private readonly DevJobsContext _context;
+        public JobApplicationsController(DevJobsContext context)
+        {
+            _context = context;
+        }
+
+        // POST api/job-vacancies/4/applications
         [HttpPost]
         public IActionResult Post(int id, AddJobApplicationInputModel model)
         {
-            
+            var jobVacancy = _context.JobVacancies
+            .SingleOrDefault(jv => jv.Id == id);
+
+            if (jobVacancy == null)
+                return NotFound();
+
+            var application = new JobApplication(
+                model.ApplicantName,
+                model.ApplicantEmail,
+                id
+            );
+
+            jobVacancy.Applications.Add(application);
+
             return NoContent();
         }
     }
