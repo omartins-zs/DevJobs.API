20f6c16d25836aa41a876d5877eef60a041b8441
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-11 13:10:36 -0300

    Adicionando Repository no JobApplications

--- Controllers/JobApplicationsController.cs
+++ Controllers/JobApplicationsController.cs
@@ -3,6 +3,7 @@ namespace DevJobs.API.Controllers
     using DevJobs.API.Entities;
     using DevJobs.API.Models;
     using DevJobs.API.Persistence;
+    using DevJobs.API.Persistence.Repositories;
     using Microsoft.AspNetCore.Mvc;
 
     // Rota diferente Por que para criar um Application tem que ter um identificador
@@ -10,18 +11,17 @@ namespace DevJobs.API.Controllers
     [ApiController]
     public class JobApplicationsController : ControllerBase
     {
-        private readonly DevJobsContext _context;
-        public JobApplicationsController(DevJobsContext context)
+        private readonly IJobVacancyRepository _repository;
+        public JobApplicationsController(IJobVacancyRepository repository)
         {
-            _context = context;
+            _repository = repository;
         }
 
         // POST api/job-vacancies/4/applications
         [HttpPost]
         public IActionResult Post(int id, AddJobApplicationInputModel model)
         {
-            var jobVacancy = _context.JobVacancies
-            .SingleOrDefault(jv => jv.Id == id);
+            var jobVacancy = _repository.GetById(id);
 
             if (jobVacancy == null)
                 return NotFound();
@@ -32,8 +32,7 @@ namespace DevJobs.API.Controllers
                 id
             );
 
-            _context.JobApplications.Add(application);
-            _context.SaveChanges();
+           _repository.AddApplication(application);
             
             return NoContent();
         }
