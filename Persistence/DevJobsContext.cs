dc6589129f1161918e0cb4f262d598eccbc5b717
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-10 23:57:38 -0300

    Ajustes no DevJobsContext para usar Entity

--- Persistence/DevJobsContext.cs
+++ Persistence/DevJobsContext.cs
@@ -5,10 +5,30 @@ namespace DevJobs.API.Persistence
 {
     public class DevJobsContext : DbContext
     {
-        public DevJobsContext(DbContextOptions<DevJobsContext> options): base(options)
+        public DevJobsContext(DbContextOptions<DevJobsContext> options) : base(options)
         {
         }
 
         public DbSet<JobVacancy> JobVacancies { get; set; }
+        public DbSet<JobApplication> JobApplications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<JobVacancy>(e =>
+            {
+                e.HasKey(jv => jv.Id);
+                // e.ToTable("tb_JobVacancies");
+
+                e.HasMany(jv => jv.Applications)
+                    .WithOne()
+                    .HasForeignKey(ja => ja.IdJobVacancy)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            builder.Entity<JobApplication>(e =>
+            {
+                e.HasKey(ja => ja.Id);
+            });
+        }
     }
 }
