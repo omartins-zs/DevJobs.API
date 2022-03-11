6ec81b6d38cc7e9889e15e7f65aa9b553e789f96
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-11 12:38:54 -0300

    Alteraçao para Sql Server e Repository

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 using DevJobs.API.Persistence;
+using DevJobs.API.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 var connectionString = builder.Configuration.GetConnectionString("DevJobsCs");
-builder.Services.AddDbContext<DevJobsContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<DevJobsContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IJobVacancyRepository, IJobVacancyRepository>();
+
+//  builder.Services.AddDbContext<DevJobsContext>(options =>
+// options.UseInMemoryDatabase("DevJobs"));   
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
